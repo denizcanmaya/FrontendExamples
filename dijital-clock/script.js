1054b797b8164d8e05e6421e0adf64613d97c684
Deniz Canmaya <denizcanmaya@gmail.com>
2025-03-18 21:48:29 +0300

    interface improvements have been made / additions have been made for background change / media quaries have been added for mobile and tablet.

--- dijital-clock/script.js
+++ dijital-clock/script.js
@@ -2,6 +2,11 @@ const hour = document.getElementById("hour");
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
 const amPm = document.getElementById("ampm");
+const body = document.querySelector("body");
+
+const whiteBg = document.querySelector(".white");
+const blueBg = document.querySelector(".blue");
+const redBg = document.querySelector(".red");
 
 timeUpdate();
 setInterval(timeUpdate,1000);
@@ -10,6 +15,18 @@ function timeUpdate() {
     const date = new Date();
     hour.innerText = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
     minutes.innerText = date.getMinutes();
-    seconds.innerText = date.getSeconds();
+    seconds.innerText = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
     amPm.innerText = date.getHours() >= 12 ? 'PM' : 'AM';
 };
+
+function changeBackground(colorClass, imageUrl) {
+    const element = document.querySelector(colorClass);
+    element.addEventListener("click", () => {
+        body.style.background = `url('${imageUrl}') no-repeat center center`;
+        body.style.backgroundSize = "cover";
+    });
+}
+
+changeBackground(".white", "assets/background2.jpg");
+changeBackground(".blue", "assets/background3.jpg");
+changeBackground(".red", "assets/background4.jpg");
